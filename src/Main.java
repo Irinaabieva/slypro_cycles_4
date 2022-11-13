197680104e26222f4d350535417bfd15b65b02b1
Irinaabieva <irinaabieva03@gmail.com>
2022-11-13 13:23:08 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
+        int sum = 0;
+        int save = 15_000;
+        for (int i = 1; sum <= 12_000_000; i++){
+            sum += save + (sum * 0.07);
+            System.out.println("Месяц " + i + " сумма накоплений равна " + sum);
+        }
     }
 }
