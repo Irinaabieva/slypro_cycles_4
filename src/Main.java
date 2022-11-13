59a4538b75d8a12ab9b4436f9dd4a1050fef4b0d
Irinaabieva <irinaabieva03@gmail.com>
2022-11-13 19:33:09 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        int start = 1822;
+        int end = 2100;
+        while (start <= end) {
+            start++;
+            if (start % 79 == 0) {
+            System.out.println(start);
+            }
+        }
     }
 }
